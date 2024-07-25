ca7e935ecf35f618239eecad6985a463e3097f48
Sphcnn <batus-2005@hotmail.com>
2024-07-25 14:24:33 +0300

    Proje dosyası ekle.

---
+++ arka_plan_silme_deneme2/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace arka_plan_silme_deneme2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectPictureButton = new System.Windows.Forms.Button();
+            this.SelectBackground = new System.Windows.Forms.Button();
+            this.ExtractButton = new System.Windows.Forms.Button();
+            this.SelectPicture_picturebox = new System.Windows.Forms.PictureBox();
+            this.SelectBackground_PictureBox = new System.Windows.Forms.PictureBox();
+            this.Showresult_Picturebox = new System.Windows.Forms.PictureBox();
+            this.tolerans_numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.SelectPicture_picturebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SelectBackground_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Showresult_Picturebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tolerans_numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SelectPictureButton
+            // 
+            this.SelectPictureButton.Location = new System.Drawing.Point(43, 318);
+            this.SelectPictureButton.Name = "SelectPictureButton";
+            this.SelectPictureButton.Size = new System.Drawing.Size(108, 23);
+            this.SelectPictureButton.TabIndex = 0;
+            this.SelectPictureButton.Text = "Resim Ekle";
+            this.SelectPictureButton.UseVisualStyleBackColor = true;
+            this.SelectPictureButton.Click += new System.EventHandler(this.SelectPictureButton_Click);
+            // 
+            // SelectBackground
+            // 
+            this.SelectBackground.Location = new System.Drawing.Point(659, 318);
+            this.SelectBackground.Name = "SelectBackground";
+            this.SelectBackground.Size = new System.Drawing.Size(108, 23);
+            this.SelectBackground.TabIndex = 1;
+            this.SelectBackground.Text = "Arka Planı seç";
+            this.SelectBackground.UseVisualStyleBackColor = true;
+            this.SelectBackground.Click += new System.EventHandler(this.SelectBackground_Click);
+            // 
+            // ExtractButton
+            // 
+            this.ExtractButton.Location = new System.Drawing.Point(84, 510);
+            this.ExtractButton.Name = "ExtractButton";
+            this.ExtractButton.Size = new System.Drawing.Size(108, 23);
+            this.ExtractButton.TabIndex = 2;
+            this.ExtractButton.Text = "Ayıkla";
+            this.ExtractButton.UseVisualStyleBackColor = true;
+            this.ExtractButton.Click += new System.EventHandler(this.ExtractButton_Click);
+            // 
+            // SelectPicture_picturebox
+            // 
+            this.SelectPicture_picturebox.Location = new System.Drawing.Point(12, 3);
+            this.SelectPicture_picturebox.Name = "SelectPicture_picturebox";
+            this.SelectPicture_picturebox.Size = new System.Drawing.Size(394, 309);
+            this.SelectPicture_picturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SelectPicture_picturebox.TabIndex = 3;
+            this.SelectPicture_picturebox.TabStop = false;
+            // 
+            // SelectBackground_PictureBox
+            // 
+            this.SelectBackground_PictureBox.Location = new System.Drawing.Point(435, 3);
+            this.SelectBackground_PictureBox.Name = "SelectBackground_PictureBox";
+            this.SelectBackground_PictureBox.Size = new System.Drawing.Size(394, 309);
+            this.SelectBackground_PictureBox.TabIndex = 4;
+            this.SelectBackground_PictureBox.TabStop = false;
+            // 
+            // Showresult_Picturebox
+            // 
+            this.Showresult_Picturebox.Location = new System.Drawing.Point(211, 318);
+            this.Showresult_Picturebox.Name = "Showresult_Picturebox";
+            this.Showresult_Picturebox.Size = new System.Drawing.Size(394, 309);
+            this.Showresult_Picturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Showresult_Picturebox.TabIndex = 5;
+            this.Showresult_Picturebox.TabStop = false;
+            this.Showresult_Picturebox.Click += new System.EventHandler(this.Showresult_Picturebox_Click);
+            // 
+            // tolerans_numericUpDown1
+            // 
+            this.tolerans_numericUpDown1.Location = new System.Drawing.Point(84, 458);
+            this.tolerans_numericUpDown1.Name = "tolerans_numericUpDown1";
+            this.tolerans_numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.tolerans_numericUpDown1.TabIndex = 6;
+            this.tolerans_numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 654);
+            this.Controls.Add(this.tolerans_numericUpDown1);
+            this.Controls.Add(this.Showresult_Picturebox);
+            this.Controls.Add(this.SelectBackground_PictureBox);
+            this.Controls.Add(this.SelectPicture_picturebox);
+            this.Controls.Add(this.ExtractButton);
+            this.Controls.Add(this.SelectBackground);
+            this.Controls.Add(this.SelectPictureButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SelectPicture_picturebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SelectBackground_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Showresult_Picturebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tolerans_numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectPictureButton;
+        private System.Windows.Forms.Button SelectBackground;
+        private System.Windows.Forms.Button ExtractButton;
+        private System.Windows.Forms.PictureBox SelectPicture_picturebox;
+        private System.Windows.Forms.PictureBox SelectBackground_PictureBox;
+        private System.Windows.Forms.PictureBox Showresult_Picturebox;
+        private System.Windows.Forms.NumericUpDown tolerans_numericUpDown1;
+    }
+}
+
---
+++ arka_plan_silme_deneme2/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arka_plan_silme_deneme2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void SelectPictureButton_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files| *.jpg; *.jpeg; *.png;";
+            ofd.Title = "Bir resim dosyası seçiniz";
+           
+            
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string picturefile = ofd.FileName;
+                SelectPicture_picturebox.Image = new Bitmap(ofd.FileName);
+            }
+
+
+           
+            if (SelectPicture_picturebox.Image != null)
+            {
+                
+                using (MemoryStream picturebyte = new MemoryStream())
+                {
+                    Bitmap picturebitmap = new Bitmap(SelectPicture_picturebox.Image);
+                    picturebitmap.Save(picturebyte, ImageFormat.Bmp);
+                    byte[] byteArray = picturebyte.ToArray();
+                }
+            }
+        }
+
+        private void SelectBackground_Click(object sender, EventArgs e)
+        {
+           
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files| *.jpg; *.jpeg; *.png;";
+            ofd.Title = "Bir resim dosyası seçiniz";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string picturefile = ofd.FileName;
+                SelectBackground_PictureBox.Image = new Bitmap(ofd.FileName);
+            }
+
+            if (SelectBackground_PictureBox.Image != null)
+            {
+                using (MemoryStream backgroundbyte = new MemoryStream())
+                {
+                    Bitmap backgroundbitmap = new Bitmap(SelectBackground_PictureBox.Image);
+                    byte[] byteArray = backgroundbyte.ToArray();
+                }
+            }
+        }
+
+      
+        private void ExtractButton_Click(object sender, EventArgs e)
+        {
+          
+            if (SelectPicture_picturebox.Image != null && SelectBackground_PictureBox.Image != null)
+            {
+                Bitmap img1 = new Bitmap(SelectPicture_picturebox.Image);
+                Bitmap img2 = new Bitmap(SelectBackground_PictureBox.Image);
+                Bitmap resultImg = new Bitmap(img1.Width, img1.Height);
+
+                int tolerans = (int)tolerans_numericUpDown1.Value;
+
+                  
+                // burada bitlerin belleklerini kitliyor ve doğrudan etkileşime geçiyor bu da hızlandırıyor
+                    BitmapData img1Data = img1.LockBits(new Rectangle(0, 0, img1.Width, img1.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                    BitmapData img2Data = img2.LockBits(new Rectangle(0, 0, img2.Width, img2.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                    BitmapData resultData = resultImg.LockBits(new Rectangle(0, 0, resultImg.Width, resultImg.Height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+
+
+                int bytesPerPixel = 3; // RGB formatında her piksel 3 byte
+                int byteCount = img1Data.Stride * img1Data.Height;
+                byte[] img1Pixels = new byte[byteCount];
+                byte[] img2Pixels = new byte[byteCount];
+                byte[] resultPixels = new byte[byteCount];
+
+       
+            
+             
+                
+                
+                
+                
+                System.Runtime.InteropServices.Marshal.Copy(img1Data.Scan0, img1Pixels, 0, byteCount);//burada pixel verileri kopyalanıyor
+                System.Runtime.InteropServices.Marshal.Copy(img2Data.Scan0, img2Pixels, 0, byteCount);
+
+             
+                
+                
+                Parallel.For(0, img1Data.Height, y =>//bu her bir pixel için işlemi paralelleştiriyor ve hızlanmaya katkı sağlıyor
+                {
+                    int yOffset = y * img1Data.Stride;
+                    for (int x = 0; x < img1Data.Width * bytesPerPixel; x += bytesPerPixel)
+                    {
+                        int rDiff = Math.Abs(img1Pixels[yOffset + x + 2] - img2Pixels[yOffset + x + 2]);
+                        int gDiff = Math.Abs(img1Pixels[yOffset + x + 1] - img2Pixels[yOffset + x + 1]);
+                        int bDiff = Math.Abs(img1Pixels[yOffset + x] - img2Pixels[yOffset + x]);
+
+                        if (rDiff * 1.7 <= tolerans && gDiff * 1.7 <= tolerans && bDiff * 1.7 <= tolerans)
+                        {
+                            resultPixels[yOffset + x] = 0;
+                            resultPixels[yOffset + x + 1] = 0;
+                            resultPixels[yOffset + x + 2] = 0;
+                        }
+                        else
+                        {
+                            resultPixels[yOffset + x] = img1Pixels[yOffset + x];
+                            resultPixels[yOffset + x + 1] = img1Pixels[yOffset + x + 1];
+                            resultPixels[yOffset + x + 2] = img1Pixels[yOffset + x + 2];
+                        }
+                    }
+                });
+
+                System.Runtime.InteropServices.Marshal.Copy(resultPixels, 0, resultData.Scan0, byteCount);
+
+                img1.UnlockBits(img1Data);//kitlenen bitmap bellekleri burada geri açlılıyor
+                img2.UnlockBits(img2Data);
+                resultImg.UnlockBits(resultData);
+
+                Showresult_Picturebox.Image = resultImg;
+                img1.Dispose();
+                img2.Dispose();
+           }
+            else
+            {
+              MessageBox.Show("Lütfen iki resmi de yükleyin.");
+            }
+     }
+
+
+
+
+
+
+
+
+        private void Showresult_Picturebox_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            tolerans_numericUpDown1.Maximum = 250;
+            tolerans_numericUpDown1.Minimum = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
